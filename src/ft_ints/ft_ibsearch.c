df44e4e3be151639f6d8b7b157c80b5722610cd9
Paul Arden <parden@made-f0Ar11s9.clusters.42paris.fr>
2024-09-22 15:42:14 +0200

    now with operational (?) bsearch

--- src/ft_ints/ft_ibsearch.c
+++ src/ft_ints/ft_ibsearch.c
@@ -6,7 +6,7 @@
 /*   By: parden <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 22:09:22 by parden            #+#    #+#             */
-/*   Updated: 2024/09/21 22:17:01 by parden           ###   ########.fr       */
+/*   Updated: 2024/09/22 15:09:53 by parden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ int	*ft_ibsearch(int key, int *arr, int size)
 {
 	int	lo;
 	int	hi;
+	int	mid;
 
-	lo = 0;
-	hi = size - 1;
-	while (lo < hi)
+	lo = -1;
+	hi = size;
+	while (hi - lo > 1)
 	{
-		if (arr[(lo + hi) / 2] < key)
-			lo = (lo + hi) / 2 + 1;
-		else if (arr[(lo + hi) / 2] > key)
-			hi = (lo + hi) / 2 - 1;
+		mid = lo + (hi - lo) / 2;
+		if (arr[mid] > key)
+			hi = mid;
 		else
-			return (arr + (lo + hi) / 2);
+			lo = mid;
 	}
-	return (NULL);
+	if (lo == -1 || arr[lo] != key)
+		return (NULL);
+	return (arr + lo);
 }
